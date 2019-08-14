a39ceec8f9fb05e17f387353572b14155cc5c59e
priyatham <saipriyathamvarma@gmail.com>
2019-08-14 11:11:04 -0400

    Added the marks demo version

---
+++ src/com/MarksDemo.java
@@ -0,0 +1,39 @@
+package com;
+
+public class MarksDemo {
+	
+	int Math,Hindi,English,Science;
+	String name;
+	public int getMath() {
+		return Math;
+	}
+	public void setMath(int math) {
+		Math = math;
+	}
+	public int getHindi() {
+		return Hindi;
+	}
+	public void setHindi(int hindi) {
+		Hindi = hindi;
+	}
+	public int getEnglish() {
+		return English;
+	}
+	public void setEnglish(int english) {
+		English = english;
+	}
+	public int getScience() {
+		return Science;
+	}
+	public void setScience(int science) {
+		Science = science;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
