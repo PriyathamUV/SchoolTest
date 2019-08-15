7ec3288f16b0accdac40ab2d295f206b3dcb9863
Dilip Reddy <Dilip Reddy@192.168.2.239>
2019-08-14 22:19:31 -0400

    student added

---
+++ src/com/Student.java
@@ -0,0 +1,21 @@
+package com;
+
+public class Student 
+{
+String Name;
+int Number;
+public String getName() {
+	return Name;
+}
+public void setName(String name) {
+	Name = name;
+}
+public int getNumber() {
+	return Number;
+}
+public void setNumber(int number) {
+	Number = number;
+}
+
+
+}
